abab28d1d014997ffdbe8cea8000f4509cd0c80c
Andrey Podovinov <andrey.podovinov@iqtek.ru>
2024-03-07 02:05:14 +0600

    added CharactersDetail component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { Navigate, type RouteObject } from 'react-router-dom';
 import Characters from 'components/Characters';
+import CharacterDetail from 'components/CharacterDetail';
 
 const appRoutes: RouteObject[] = [
   { index: true, element: <Navigate to="characters" /> },
   { path: 'characters', element: <Characters /> },
-  { path: 'characters/:id', element: <div>you are here</div> },
+  { path: 'characters/:id', element: <CharacterDetail /> },
   { path: 'comics', element: <div>Comics</div> },
   { path: '*', element: <div>Not found</div> }
 ];
