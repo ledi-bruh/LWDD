796eface17f8daaaf4689b244f4635a4c1720916
Andrey Podovinov <andrey.podovinov@iqtek.ru>
2024-03-07 02:07:16 +0600

    added NotFound component

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import classes from './NotFound.module.css';
+
+const NotFound: FC = () => {
+  return <div className={classes.notFoundContainer}>Page is not found 🐈</div>;
+};
+
+export default NotFound;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,14 @@
 import { Navigate, type RouteObject } from 'react-router-dom';
 import Characters from 'components/Characters';
 import CharacterDetail from 'components/CharacterDetail';
+import NotFound from 'components/NotFound';
 
 const appRoutes: RouteObject[] = [
   { index: true, element: <Navigate to="characters" /> },
   { path: 'characters', element: <Characters /> },
   { path: 'characters/:id', element: <CharacterDetail /> },
   { path: 'comics', element: <div>Comics</div> },
-  { path: '*', element: <div>Not found</div> }
+  { path: '*', element: <NotFound /> }
 ];
 
 export default appRoutes;
