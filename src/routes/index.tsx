0123139c5c6f2e9e28795d32f0ceeb56a76051fb
Andrey Podovinov <andrey.podovinov@iqtek.ru>
2024-03-07 11:53:09 +0600

    added ComicsDetail component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,14 @@ import Characters from 'components/Characters';
 import CharacterDetail from 'components/CharacterDetail';
 import NotFound from 'components/NotFound';
 import Comics from 'components/Comics';
+import ComicsDetail from 'components/ComicsDetail';
 
 const appRoutes: RouteObject[] = [
   { index: true, element: <Navigate to="characters" /> },
   { path: 'characters', element: <Characters /> },
   { path: 'characters/:id', element: <CharacterDetail /> },
   { path: 'comics', element: <Comics /> },
+  { path: 'comics/:id', element: <ComicsDetail /> },
   { path: '*', element: <NotFound /> }
 ];
 
