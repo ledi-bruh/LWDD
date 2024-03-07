16fb893e7a82c099adeb9ae9f0e026520f340934
Andrey Podovinov <andrey.podovinov@iqtek.ru>
2024-03-07 11:08:38 +0600

    added Comics component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,13 @@ import { Navigate, type RouteObject } from 'react-router-dom';
 import Characters from 'components/Characters';
 import CharacterDetail from 'components/CharacterDetail';
 import NotFound from 'components/NotFound';
+import Comics from 'components/Comics';
 
 const appRoutes: RouteObject[] = [
   { index: true, element: <Navigate to="characters" /> },
   { path: 'characters', element: <Characters /> },
   { path: 'characters/:id', element: <CharacterDetail /> },
-  { path: 'comics', element: <div>Comics</div> },
+  { path: 'comics', element: <Comics /> },
   { path: '*', element: <NotFound /> }
 ];
 
