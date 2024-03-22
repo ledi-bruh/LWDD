0759eca45c49ce50b1b8119fbc7133d5999bf4a7
Andrey Podovinov <andrey.podovinov@iqtek.ru>
2024-03-23 02:55:57 +0600

    update CardDetail with corresponding routes

--- src/routes/CharacterDetail/CharacterDetail.tsx
+++ src/routes/CharacterDetail/CharacterDetail.tsx
@@ -1,11 +1,45 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import api from 'api';
 import CartDetail from 'components/CartDetail';
+import { Card } from 'types';
+import { Comic } from 'api/types';
 
 const CharacterDetail: FC = () => {
+  const { id } = useParams() as { id: string };
+  const [card, setCard] = useState({} as Card);
+  const [subCards, setSubCards] = useState([] as Card[]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const character = await api.characters.getById(id);
+      const comics = await api.characters.getComics(id);
+      setCard({
+        id: character.id.toString(),
+        image: `${character.thumbnail.path}.${character.thumbnail.extension}`,
+        title: character.name,
+        description: character.description
+      });
+      setSubCards(
+        comics.results.map((comic: Comic) => {
+          return {
+            id: comic.id.toString(),
+            image: `${comic.thumbnail.path}.${comic.thumbnail.extension}`,
+            title: comic.title,
+            description: comic.description
+          };
+        })
+      );
+    };
+
+    fetchData();
+  }, [id]);
+
   return (
     <CartDetail
-      baseUrl={'/characters'}
+      card={card}
       subCardsTitle={'Comics'}
+      subCards={subCards}
       subCardsUrl={'/comics'}
     />
   );
--- src/routes/ComicsDetail/ComicsDetail.tsx
+++ src/routes/ComicsDetail/ComicsDetail.tsx
@@ -1,11 +1,45 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import api from 'api';
 import CartDetail from 'components/CartDetail';
+import { Card } from 'types';
+import { Character } from 'api/types';
 
 const ComicsDetail: FC = () => {
+  const { id } = useParams() as { id: string };
+  const [card, setCard] = useState({} as Card);
+  const [subCards, setSubCards] = useState([] as Card[]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const comic = await api.comics.getById(id);
+      const characters = await api.comics.getCharacters(id);
+      setCard({
+        id: comic.id.toString(),
+        image: `${comic.thumbnail.path}.${comic.thumbnail.extension}`,
+        title: comic.title,
+        description: comic.description
+      });
+      setSubCards(
+        characters.results.map((character: Character) => {
+          return {
+            id: character.id.toString(),
+            image: `${character.thumbnail.path}.${character.thumbnail.extension}`,
+            title: character.name,
+            description: character.description
+          };
+        })
+      );
+    };
+
+    fetchData();
+  }, [id]);
+
   return (
     <CartDetail
-      baseUrl={'/comics'}
+      card={card}
       subCardsTitle={'Characters'}
+      subCards={subCards}
       subCardsUrl={'/characters'}
     />
   );
